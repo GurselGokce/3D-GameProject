d92e9ed1b8f2bc4314d37fba7d8300a17b23853a
AyoubAouraghe <45266255+AyoubAouraghe@users.noreply.github.com>
2020-05-29 21:34:35 +0200

    movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,37 +8,40 @@ public class PlayerController : MonoBehaviour {
     Camera cam;
     PlayerMove move;
     public Animator animator;
+    private float speed;
     // Start is called before the first frame update
     void Start() {
         cam = Camera.main;
         move = GetComponent<PlayerMove>();
-        
+        speed = 1f;
 
     }
 
     // Update is called once per frame
     void Update() {
-        
-        if (Input.GetMouseButtonDown(0))
-        {
-            animator.SetFloat("verticale", Input.GetAxis("Vertical"));
-            animator.SetFloat("horizontale", Input.GetAxis("Horizontal"));
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, speed * Input.GetAxis("Vertical") * Time.deltaTime); ;
+        animator.SetFloat("verticale", Input.GetAxis("Vertical"));
+        animator.SetFloat("horizontale", Input.GetAxis("Horizontal"));
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    animator.SetFloat("verticale", Input.GetAxis("Vertical"));
+        //    animator.SetFloat("horizontale", Input.GetAxis("Horizontal"));
+        //    Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        //    RaycastHit hit;
 
-            if(Physics.Raycast(ray, out hit, 100, movementMask))
-            {
-                move.Move(hit.point);
-                
-                //Debug.Log("Hit" + hit.collider.name +" " + hit.point);
-                //Move player to what hits
+        //    if(Physics.Raycast(ray, out hit, 100, movementMask))
+        //    {
+        //        move.Move(hit.point);
 
-                //Stop focusing object
+        //        //Debug.Log("Hit" + hit.collider.name +" " + hit.point);
+        //        //Move player to what hits
+
+        //        //Stop focusing object
 
 
-            }
-        }
-    
-        
+        //    }
+        //}
+
+
     }
 }
