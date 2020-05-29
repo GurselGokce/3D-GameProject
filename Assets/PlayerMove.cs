c6a5230b38a6d25914ee0e8a8650a009a63f13cd
AyoubAouraghe <45266255+AyoubAouraghe@users.noreply.github.com>
2020-05-29 20:53:18 +0200

    animatie

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -7,12 +7,13 @@ using UnityEngine.AI;
 public class PlayerMove : MonoBehaviour {
 
     NavMeshAgent agent;
+   
 
     // Start is called before the first frame update
     void Start() {
 
         agent = GetComponent<NavMeshAgent>();
-    
+        
 
 
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,30 +7,35 @@ public class PlayerController : MonoBehaviour {
     public LayerMask movementMask;
     Camera cam;
     PlayerMove move;
+    public Animator animator;
     // Start is called before the first frame update
     void Start() {
         cam = Camera.main;
         move = GetComponent<PlayerMove>();
-
         
+
     }
 
     // Update is called once per frame
     void Update() {
+        
         if (Input.GetMouseButtonDown(0))
         {
+            animator.SetFloat("verticale", Input.GetAxis("Vertical"));
+            animator.SetFloat("horizontale", Input.GetAxis("Horizontal"));
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if(Physics.Raycast(ray, out hit, 100, movementMask))
             {
                 move.Move(hit.point);
+                
                 //Debug.Log("Hit" + hit.collider.name +" " + hit.point);
                 //Move player to what hits
 
                 //Stop focusing object
 
-            
+
             }
         }
     
