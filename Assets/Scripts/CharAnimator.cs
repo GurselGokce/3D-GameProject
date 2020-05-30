fa4973124c5f28462e298bf0296cd6f866bd64d5
AyoubAouraghe <45266255+AyoubAouraghe@users.noreply.github.com>
2020-05-30 23:45:07 +0200

    Animation bij wandelen

---
+++ Assets/Scripts/CharAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    const float motionSmoothTime = 0.1f;
+
+    NavMeshAgent agent;
+    Animator animator;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedPer = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPer", speedPer, motionSmoothTime, Time.deltaTime);
+    }
+}
