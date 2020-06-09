fe208e63ca93d9ab2be57f004a96ce35916084ef
AyoubAouraghe <45266255+AyoubAouraghe@users.noreply.github.com>
2020-06-10 01:20:09 +0200

    deuren werken

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if(collider.tag == "Door")
+        {
+            Animator anim = collider.GetComponentInChildren<Animator>();
+            if (Input.GetKeyDown(KeyCode.E))
+                anim.SetTrigger("OpenClose");
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Slidingdoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slidingdoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject trigger;
+    public GameObject door;
+
+    Animator anim;
+    void Start()
+    {
+        anim = door.GetComponent<Animator>();
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        Debug.Log("deur");
+        if (collider.gameObject.tag == "Player")
+        {
+            //Animator anim = collider.GetComponentInChildren<Animator>();
+            Debug.Log("ik ben bij de deur");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("OpenClose");
+                anim.SetTrigger("OpenClose");
+            }
+                
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
