5c6c3fd02a1abaa4e74449e1adc16440d4d22a97
AyoubAouraghe <45266255+AyoubAouraghe@users.noreply.github.com>
2020-06-11 01:54:24 +0200

    gameoverscherem

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
 	public int maxHealth = 100;
 	public int currentHealth;
-
+	public GameObject instructions;
 	public HealthBar healthBar;
 
 	// Start is called before the first frame update
@@ -19,23 +20,36 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.H))
-		{
-			Debug.Log("takedamage");
-			TakeDamage(20);
-		}
+		//if (Input.GetKeyDown(KeyCode.H))
+		//{
+		//	Debug.Log("takedamage");
+		//	TakeDamage(20);
+		//}
 	}
     void OnCollisionEnter(Collision collision)
     {
         
         if (collision.gameObject.tag == "Enemy")
         {
-			Debug.Log("takedamage");
+			//Debug.Log("takedamage");
 			TakeDamage(20);
+			if (currentHealth <= 0)
+			{
+				Debug.Log("ik ben dood");
+				instructions.SetActive(true);
+				StartCoroutine(screenDelay());
+				
+			}
 		}
+		
     }
+	IEnumerator screenDelay()
+	{
+		yield return new WaitForSeconds(3f);
+		SceneManager.LoadScene("MainMenu");
+	}
 
-    void TakeDamage(int damage)
+	void TakeDamage(int damage)
 	{
 		currentHealth -= damage;
 
