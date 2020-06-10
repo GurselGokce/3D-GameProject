5f620bf4a55b5411e0cceeada956a9bccd966449
AyoubAouraghe <45266255+AyoubAouraghe@users.noreply.github.com>
2020-06-11 00:58:04 +0200

    spawn

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject enemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+    public int totaal = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+    IEnumerator EnemyDrop()
+    {
+        while(enemyCount < totaal)
+        {
+            xPos = Random.Range(-20, 20);
+            zPos = Random.Range(-3, 3);
+            Instantiate(enemy, new Vector3(xPos, 3, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.P))
+        //{
+        //    enemyCount -= 5;
+        //}
+    }
+
+    
+
+   
+}
